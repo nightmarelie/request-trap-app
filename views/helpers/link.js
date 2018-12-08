a08c917a2bbc9bb5086a40326668e629bf37d4df
Aleksey Kryvtsov <nightmarelie@gmail.com>
2018-12-08 21:31:23 +0200

    Added logic to instruction page

--- views/helpers/link.js
+++ views/helpers/link.js
@@ -3,6 +3,7 @@ module.exports = hbs => (text, options) => {
     for(const prop in options.hash) {
         attrs.push(`${prop} = "/${text}${options.hash[prop]}"`);
     }
+    attrs.push(`id = "${text}"`);
     return new hbs.SafeString(
         `<a ${attrs.join(" ")}>${text}</a>`
     );
